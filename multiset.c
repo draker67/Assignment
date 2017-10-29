8ebc6202062c3be4863fe39796f47049dacf2eaf
Sarath Raveendran <31508468+draker67@users.noreply.github.com>
2017-10-29 18:15:56 +0530

    Update multiset.c
    -added GUI
    -corrected deletion

--- multiset.c
+++ multiset.c
@@ -2,18 +2,66 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<windows.h>
+#include<time.h>
+
+void delay(unsigned int ms)
+{
+	clock_t goal=ms+clock();
+	while(goal>clock());
+}
+
+
+void SetColorAndBackground(int ForgC,int BackC)
+{
+
+WORD wColor=((BackC & 0x0F) << 4)+(ForgC & 0x0F);;
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),wColor);
+return;
+
+}
+
+
+
+
+
+COORD coordinate={0,0};
+void gotoxy(int x,int y)
+{
+	coordinate.X=x;
+	coordinate.Y=y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coordinate);
+}
+
+
+
 typedef struct node
 {
     char data[10];
     int count;
     struct node *link;
 }NODE;
-void insert(NODE *header,char data[])
+void insert(NODE *header)
 {
-    NODE *ptr,*new;
+    NODE *ptr,*n;
     int flag;
+    char data[10];
     ptr=header;
     flag=0;
+    system("cls");
+    SetColorAndBackground(15,1);
+
+	gotoxy(25,17);
+
+	printf("Enter the element to be inserted: ");
+    gotoxy(60,17);
+    printf("{ ");
+
+
+	scanf(" %s",data);
+    gotoxy(63,17);
+	printf("}");
+
     while(ptr->link!=NULL)
     {
         ptr=ptr->link;
@@ -26,75 +74,99 @@ void insert(NODE *header,char data[])
     }
     if(flag==0)
     {
-        new=(NODE*)malloc(sizeof(NODE));
-        strcpy(new->data,data);
-        new->count=1;
-        new->link=NULL;
-        ptr->link=new;
-        printf("\n%s inserted",data);
+        n=(NODE*)malloc(sizeof(NODE));
+        strcpy(n->data,data);
+        n->count=1;
+        n->link=NULL;
+        ptr->link=n;
+        gotoxy(27,19);
+        printf("%s inserted",data);
     }
     else
     {
         increment:
         ptr->count++;
-        printf("\n%s inserted",data);
+        gotoxy(27,19);
+        printf("%s inserted",data);
     }
+
+//goto hello;
 }
-void delete(NODE *header,char data[])
+
+void delete1(NODE *header)
 {
     NODE *ptr,*ptr1;
     int flag=0;
-    ptr=header->link;
-    if(ptr==NULL)
+    char data[5];
+    system("cls");
+	SetColorAndBackground(15,1);
+
+	ptr=header;
+	gotoxy(25,17);
+    printf("Enter the element to be deleted ");
+	gotoxy(60,17);
+	scanf(" %s",data);
+	if(ptr==NULL)
     {
-        printf("\nThe bag is empty");
+    	gotoxy(25,19);
+        printf("The bag is empty");
     }
     else
     {
         while(ptr->link!=NULL)
         {
+            ptr1=ptr;
+            ptr=ptr->link;
             if(strcmp(ptr->data,data)==0)
             {
                 flag=1;
                 break;
             }
-            ptr1=ptr;
-            ptr=ptr->link;
+
         }
         if(flag==0)
         {
-            printf("\n%s is not present in the bag",data);
+        	gotoxy(25,19);
+            printf("%s is not present in the bag",data);
         }
         else
         {
             (ptr->count)--;
-            printf("\nOne %s has been deleted from the bag",data);
+            gotoxy(25,19);
+            printf("One %s has been deleted from the bag",data);
             if(ptr->count==0)
             {
-                printf("\nThere are no %s's remaining in the bag",data);
+                gotoxy(25,19);
+				printf("There are no %s's remaining in the bag",data);
                 ptr1->link=ptr->link;
                 free(ptr);
             }
             else
             {
-                printf("\nThere is/are %d %s's remaining in the bag",ptr->count,data);
+                gotoxy(25,20);
+				printf("There is/are %d %s's remaining in the bag",ptr->count,data);
             }
         }
     }
+  //  goto hello;
 }
 void display(NODE *header)
 {
     NODE *ptr;
     char data[10];
     int i;
-    ptr=header->link;
+    SetColorAndBackground(15,1);
+	ptr=header->link;
+    system("cls");
     if(ptr==NULL)
     {
-        printf("\nThe bag is empty");
+    	gotoxy(25,17);
+        printf("The bag is empty");
     }
     else
     {   //
-        printf("\nThe bag is:\n{");
+    	gotoxy(25,17);
+        printf("The bag is:{");
         while(ptr!=NULL)
         {
             if(header->link==ptr)
@@ -112,47 +184,83 @@ void display(NODE *header)
         //
 
 
-        printf("}\n");
+        printf("}");
     }
 }
+
+void end()
+{	int i;
+	SetColorAndBackground(15,1);
+	for(i=1;i<40;i++)
+	{
+		system("cls");
+		gotoxy(35,3);
+		printf("DEVELOPERS OF THIS PROGRAM......");
+		gotoxy(i+6,8);
+		printf("NANDAGOPAL");
+		gotoxy(80-i+4,10);
+		printf("SARATH");
+		gotoxy(i+6,12);
+		printf("SIDHARTH");
+		gotoxy(80-i+4,14);
+		printf("VIVEK");
+		delay(30);
+
+	}
+}
+
+
+
 int main()
 {
     NODE *header;
-    int ch;
+    int i, ch;
     char data[10];
     header=(NODE*)malloc(sizeof(NODE));
     strcpy(header->data,"NULL");
     header->count=0;
     header->link=NULL;
-    printf("\nBag/multiset implemented using linked list");
-    while(1)
+    SetColorAndBackground(15,1);
+
+	system("cls");
+	hello:
     {
-        printf("\n\nPress\n1. Insert an element into the bag\n2. Delete an element from the bag\n3. Display the elements of the bag\n4. Exit\n");
-        scanf("%d",&ch);
-        if(ch==1)
+    	gotoxy(35,4);
+        printf("Bag/multiset implementation using linked list");
+    	gotoxy(40,8);
+		printf("1. Insert an element into the bag");
+		gotoxy(40,9);
+		printf("2. Delete an element from the bag");
+		gotoxy(40,10);
+		printf("3. Display the elements of the bag");
+		gotoxy(40,11);
+		printf("4. Exit");
+        gotoxy(40,15);
+        printf("your choice:");
+		scanf("%d",&ch);
+        switch(ch)
         {
-            printf("\nEnter the element to be inserted ");
-            scanf(" %s",data);
-            insert(header,data);
-        }
-        else if(ch==2)
-        {
-            printf("\nEnter the element to be deleted ");
-            scanf(" %s",data);
-            delete(header,data);
-        }
-        else if(ch==3)
-        {
-            display(header);
-        }
-        else if(ch==4)
-        {
-            exit(0);
-        }
-        else
-        {
-            printf("\nWrong input");
-        }
+
+			case 1: insert(header);
+					getchar();
+					break;
+
+			case 2: delete1(header);
+					getchar();
+ 					break;
+
+			case 3: display(header);
+            		getchar();
+        			break;
+
+        	case 4: end();
+					exit(0);
+					break;
+        	default :printf("Wrong input");
+
+
+        };
+    goto hello;
     }
     return 0;
 }
